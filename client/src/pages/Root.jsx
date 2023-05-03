3a620e675f5b5e0bc16e81883ed18c082de40725
dongwook98 <ehddnr8563@gmail.com>
2023-05-03 22:45:26 +0900

    💬 Comment : 푸터 컴포넌트에 반드시 a태그 넣어주세요!(저작권)

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -6,7 +6,13 @@ export default function Root() {
     <div>
       <nav>Navbar 컴포넌트 들어올 자리</nav>
       <Outlet />
-      <footer>Footer 컴포넌트 들어올 자리</footer>
+      <footer>
+        Footer 컴포넌트 들어올 자리
+        <a href="https://www.flaticon.com/kr/free-icons/" title="로켓 아이콘">
+          로켓 아이콘 제작자: Freepik - Flaticon
+        </a>
+        {/* 푸터에 위에 a링크 추가해주세요! 아이콘 저작권 입니다!!! 꼭!! */}
+      </footer>
     </div>
   );
 }
