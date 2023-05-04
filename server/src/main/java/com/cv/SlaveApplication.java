66144405d3658f4d9391b0cbcfcbe5b324523376
tkfkdgowksel <tkfkdgowksel@gmail.com>
2023-05-04 11:43:14 +0900

    💡 Init : BE 초기 환경설정

---
+++ server/src/main/java/com/cv/SlaveApplication.java
@@ -0,0 +1,13 @@
+package com.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaveApplication.class, args);
+	}
+
+}
