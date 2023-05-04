8ffe41e219d9cec2050eb4f7c33d35649026b7ab
dohyun98 <kch9554@naver.com>
2023-05-04 18:05:51 +0900

    🔨 Refactor : MyPage

--- client/src/components/MyPage/Profile.jsx
+++ client/src/components/MyPage/Profile.jsx
@@ -1,31 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './profile.module.css';
 
 const Profile = () => {
   const skills = ['JavaScript', 'React', 'Redux'];
+  const [infoUpdata, setInfoUpdata] = useState(false);
 
   return (
     <div className={styles.container}>
-      <div className={styles.proCard}>
-        <div className={styles.userInfo}>
-          <div className={styles.profilePic}>
-            <img
-              className={styles.pic}
-              src="https://mediaim.expedia.com/localexpert/1391601/fe50a3dc-b95f-4815-a331-05cbbc16d855.jpg?impolicy=resizecrop&rw=1005&rh=565"
-            />
+      {infoUpdata ? (
+        <div className={styles.proCard}>
+          <div className={styles.userInfo}>
+            <div className={styles.profilePic}>
+              <img
+                className={styles.pic}
+                src="https://mediaim.expedia.com/localexpert/1391601/fe50a3dc-b95f-4815-a331-05cbbc16d855.jpg?impolicy=resizecrop&rw=1005&rh=565"
+              />
+              <div>+</div>
+            </div>
+            <div className={styles.proInfo}>
+              <div className={styles.info}>
+                <span>김도현</span>
+              </div>
+              <span className={styles.emailNum}>kdohyn98@github.com</span>
+              <span>010-0000-0000</span>
+            </div>
           </div>
-          <div className={styles.proInfo}>
-            <div className={styles.infos}>
-              <span className={styles.info}>이름 : </span>
-              <span>김도현</span>
-            </div>
-            <div className={styles.infos}>
-              <span className={styles.info}>email : </span>
-              <span>kdohyn98@github.com</span>
-            </div>
+          <div className={styles.updata}>
+            <button
+              className={styles.updataBtn}
+              onClick={() => setInfoUpdata(false)}
+            >
+              취소
+            </button>
+            <button
+              className={styles.updataBtn}
+              onClick={() => setInfoUpdata(false)}
+            >
+              저장
+            </button>
           </div>
         </div>
-        <div className={styles.skillTags}>
+      ) : (
+        <div className={styles.proCard}>
+          <div className={styles.userInfo}>
+            <div className={styles.profilePic}>
+              <img
+                className={styles.pic}
+                src="https://mediaim.expedia.com/localexpert/1391601/fe50a3dc-b95f-4815-a331-05cbbc16d855.jpg?impolicy=resizecrop&rw=1005&rh=565"
+              />
+            </div>
+            <div className={styles.proInfo}>
+              <div className={styles.info}>
+                <span>김도현</span>
+              </div>
+              <span className={styles.emailNum}>kdohyn98@github.com</span>
+              <span>010-0000-0000</span>
+            </div>
+          </div>
+          <div className={styles.updata}>
+            <button
+              className={styles.updataBtn}
+              onClick={() => setInfoUpdata(true)}
+            >
+              수정
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Profile;
+{
+  /* <div className={styles.skillTags}>
           {skills.map((el, index) => {
             return (
               <span className={styles.skillTag} key={index}>
@@ -33,15 +80,5 @@ const Profile = () => {
               </span>
             );
           })}
-        </div>
-      </div>
-      <div className={styles.infoUpdate}>
-        <span>비밀번호 변경</span>
-        <span> / </span>
-        <span>수정하기</span>
-      </div>
-    </div>
-  );
-};
-
-export default Profile;
+        </div> */
+}
--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Profile from '../components/MyPage/Profile';
 import styles from './mypage.module.css';
+import Info from '../components/MyPage/Info';
 
 const MyPage = () => {
   return (
     <div className={styles.container}>
       <Profile />
+      <Info />
     </div>
   );
 };
