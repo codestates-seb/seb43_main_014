01e7ed0cdfd3952bb4ebe7b7645f41ca182e59a3
강동욱 <119502777+dongwook98@users.noreply.github.com>
2023-05-04 18:05:36 +0900

    Revert "5/4 풀리퀘"

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
