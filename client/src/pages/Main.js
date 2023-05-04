e4597c405efff4ddfac5a074889644dae2a5a493
orihiro <kimhijin1@naver.com>
2023-05-04 17:22:55 +0900

    🎨 Design : Header, Footer UI

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Main = () => {
   return (
     <>
-      <h1>ss</h1>
+      <Header />
+      <Footer />
     </>
   );
 };
