123ac396d52183218252a02dfe43138d909e36a8
dongwook98 <ehddnr8563@gmail.com>
2023-05-07 14:56:30 +0900

    ✨ Feat : 회원가입 유효성 검사 기능 구현

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -16,17 +16,117 @@ export default function Signup() {
     password_confirm: '',
   });
 
+  const [errors, setErrors] = useState({
+    name: '',
+    phone: '',
+    email: '',
+    password: '',
+    password_confirm: '',
+  });
+
+  const [valid, setValid] = useState({
+    name: false,
+    phone: false,
+    email: false,
+    password: false,
+    password_confirm: false,
+  });
+  console.log(valid);
+
+  const validate = () => {
+    const newErrors = {
+      name: '',
+      phone: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+    };
+    if (!/^[\uac00-\ud7a3]+$/.test(form.name)) {
+      newErrors.name = '이름은 한글만 입력 가능합니다.';
+      setValid((prevValid) => ({ ...prevValid, name: false }));
+    } else if (!/^(?=.{2,20}$)[가-힣]{2,20}$/.test(form.name)) {
+      newErrors.name = '이름은 2~20자 사이의 한글만 입력 가능합니다.';
+      setValid((prevValid) => ({ ...prevValid, name: false }));
+    } else {
+      newErrors.name = '올바른 형식입니다.';
+      setValid((prevValid) => ({ ...prevValid, name: true }));
+    }
+
+    if (form.phone.trim() === '') {
+      newErrors.phone = '휴대폰 번호를 입력해주세요.';
+      setValid((prevValid) => ({ ...prevValid, phone: false }));
+    } else if (
+      !/^01([016789]{1}|[5]{1}[0123456789]{1})(\d{3}|\d{4})(\d{4})$/.test(
+        form.phone,
+      )
+    ) {
+      newErrors.phone = '올바른 휴대폰 번호를 입력해주세요.';
+      setValid((prevValid) => ({ ...prevValid, phone: false }));
+    } else {
+      newErrors.phone = '올바른 형식입니다.';
+      setValid((prevValid) => ({ ...prevValid, phone: true }));
+    }
+
+    if (form.email.trim() === '') {
+      newErrors.email = '이메일을 입력해주세요.';
+      setValid((prevValid) => ({ ...prevValid, email: false }));
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(form.email)) {
+      newErrors.email = '올바른 이메일 형식이 아닙니다.';
+      setValid((prevValid) => ({ ...prevValid, email: false }));
+    } else {
+      newErrors.email = '올바른 형식입니다.';
+      setValid((prevValid) => ({ ...prevValid, email: true }));
+    }
+
+    if (form.password.trim() === '') {
+      newErrors.password = '비밀번호를 입력해주세요.';
+      setValid((prevValid) => ({ ...prevValid, password: false }));
+    } else if (form.password.length < 8) {
+      newErrors.password = '비밀번호는 8자리 이상이어야 합니다.';
+      setValid((prevValid) => ({ ...prevValid, password: false }));
+    } else if (
+      !/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/.test(form.password)
+    ) {
+      newErrors.password =
+        '영문자, 숫자, 특수문자를 포함한 8글자 이상의 비밀번호를 입력해주세요.';
+      setValid((prevValid) => ({ ...prevValid, password: false }));
+    } else {
+      newErrors.password = '올바른 형식입니다.';
+      setValid((prevValid) => ({ ...prevValid, password: true }));
+    }
+
+    if (form.password_confirm.trim() === '') {
+      newErrors.password_confirm = '비밀번호를 다시 입력해주세요.';
+    } else if (form.password !== form.password_confirm) {
+      newErrors.password_confirm = '비밀번호가 일치하지 않습니다.';
+    } else {
+      newErrors.password_confirm = '올바른 형식입니다.';
+      setValid((prevValid) => ({ ...prevValid, password_confirm: true }));
+    }
+    setErrors(newErrors);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      // 유효성 검사에 성공하면 폼 데이터를 서버로 보냅니다.
+      console.log('Form data:', form);
+    }
+  };
+
   const handleChange = (e) => {
+    validate();
     const { name, value } = e.target;
     // console.log(name, e.target.value);
     console.log(form);
-    setForm({ ...form, [name]: value });
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
   };
+
   return (
     <main className={styles.container}>
       <HelloBox />
       <FormBox>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.tapMenu}>
             <Link to="/login" className={styles.login}>
               <h1>로그인</h1>
@@ -43,6 +143,10 @@ export default function Signup() {
             value={form.name}
             handleChange={handleChange}
           />
+          <div className={valid.name ? styles.successMsg : styles.errorsMsg}>
+            {errors.name}
+          </div>
+
           <LabelInput
             labelText="휴대폰 번호"
             type="text"
@@ -51,6 +155,9 @@ export default function Signup() {
             value={form.phone}
             handleChange={handleChange}
           />
+          <div className={valid.phone ? styles.successMsg : styles.errorsMsg}>
+            {errors.phone}
+          </div>
           <LabelInput
             labelText="이메일"
             type="email"
@@ -60,6 +167,9 @@ export default function Signup() {
             value={form.email}
             handleChange={handleChange}
           />
+          <div className={valid.email ? styles.successMsg : styles.errorsMsg}>
+            {errors.email}
+          </div>
           <LabelInput
             labelText="비밀번호"
             type="password"
@@ -68,6 +178,11 @@ export default function Signup() {
             value={form.password}
             handleChange={handleChange}
           />
+          <div
+            className={valid.password ? styles.successMsg : styles.errorsMsg}
+          >
+            {errors.password}
+          </div>
           <LabelInput
             isNoLabel={true}
             type="password"
@@ -76,6 +191,13 @@ export default function Signup() {
             value={form.password_confirm}
             handleChange={handleChange}
           />
+          <div
+            className={
+              valid.password_confirm ? styles.successMsg : styles.errorsMsg
+            }
+          >
+            {errors.password_confirm}
+          </div>
           <Button text="회원가입" />
         </form>
         <Oauth />
