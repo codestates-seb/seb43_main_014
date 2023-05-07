c0c10395c064ac4b4f4c7d631b0a5e5efe2bb92e
dohyun98 <kch9554@naver.com>
2023-05-07 21:34:46 +0900

    ✨ Feat : 회원 탈퇴 모달

---
+++ client/src/components/MyPage/DeleteAccount/DeleteModal/index.jsx
@@ -0,0 +1,68 @@
+import { React, useState } from 'react';
+import styles from './deleteModal.module.css';
+// role: 'dialog',
+
+const DeleteModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [check, setCheck] = useState(false);
+  const openModalHandler = () => {
+    // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+    setIsOpen(!isOpen);
+  };
+  const onClick = () => {
+    console.log('btn');
+  };
+  const onCheck = (e, check) => {
+    // setCheck(!check);
+    check = e.target.checked;
+    console.log(check);
+  };
+  return (
+    <>
+      <div className={styles.modalContainer}>
+        <button className={styles.modalBtn} onClick={openModalHandler}>
+          삭제하기
+        </button>
+        {isOpen ? (
+          <div className={styles.modalBackdrop} onClick={openModalHandler}>
+            <div
+              className={styles.modalView}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div>
+                <h4>계정 삭제</h4>
+                <div className={styles.modalGuide}>
+                  (이름) 님(email)의 계정 삭제를 선택 하였습니다.
+                </div>
+                <span>프로필 정보</span>
+                <div className={styles.modalGuideList}>
+                  <ul>
+                    <li>
+                      고객님의 이름, 이메일, 프로필 사진, 이력서를 포함한 모든
+                      개인정보를 삭제합니다.
+                    </li>
+                    <li>삭제한 정보는 복구할 수 없습니다.</li>
+                  </ul>
+                </div>
+                <div className={styles.modalGuideCheck}>
+                  <div className={styles.modalGuideCheckbox}>
+                    <input type="checkbox" onChange={(e) => onCheck(e)} />
+                  </div>
+                  <span>
+                    계정을 삭제하면 되돌릴 수 없으며, 삭제한 데이터를 복구할 수
+                    없음을 이해했습니다.
+                  </span>
+                </div>
+                <div className={styles.modalGuideBtn}>
+                  <button onClick={onClick}>계정 삭제하기</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default DeleteModal;
