1a7b985cf6715f24890e3c3fa3ba5b32d414f771
jaeyumn <wkddkgodsla@gmail.com>
2023-05-08 18:07:23 +0900

    ✨ Feat : ExceptionCode 로직 구현

--- server/src/main/java/com/cv/global/exception/ExceptionCode.java
+++ server/src/main/java/com/cv/global/exception/ExceptionCode.java
@@ -8,6 +8,11 @@ public enum ExceptionCode {
     USER_EXISTS(409, "User not exists"),
     RESUME_NOT_FOUND(404, "Resume not found");
 
+    /*
+    TODO : 예외 코드 필요 시 상황에 맞체 추가할 것
+        - 401 Unauthorized, 403 Forbidden, 404 Not Found
+        - 500 Internal Server Error, 502 Bad Gateway
+     */
 
     @Getter
     private int status;
