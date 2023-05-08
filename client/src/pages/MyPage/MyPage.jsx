dc4f70fe449ae2ca9b8c8068a85277f2a9308ef9
dohyun98 <kch9554@naver.com>
2023-05-08 22:52:08 +0900

    🔨 Refactor : mypage 수정

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -3,20 +3,28 @@ import styles from './mypage.module.css';
 import PwChange from '../../components/MyPage/PwChange/PwChange';
 import Profile from '../../components/MyPage/Profile/Profile';
 import DeleteAccount from '../../components/MyPage/DeleteAccount';
+import CvList from '../../components/MyPage/CvList';
 
 const MyPage = () => {
   return (
     <div className={styles.container}>
-      <div>
-        <h3 className={styles.profileTitle}>기본정보</h3>
+      <div className={styles.mypageItem}>
+        <h2 className={styles.profileTitle}>기본정보</h2>
         <Profile />
       </div>
-      <div>
-        <h3>비밀번호</h3>
+      <div className={styles.mypageCv}>
+        <h2>이력서</h2>
+        <CvList />
+        <div className={styles.cvList}>
+          <button>새이력서 추가</button>
+        </div>
+      </div>
+      <div className={styles.mypageItem}>
+        <h2>비밀번호</h2>
         <PwChange />
       </div>
-      <div>
-        <h3>계정 삭제</h3>
+      <div className={styles.mypageItem}>
+        <h2>계정 삭제</h2>
         <DeleteAccount />
       </div>
     </div>
