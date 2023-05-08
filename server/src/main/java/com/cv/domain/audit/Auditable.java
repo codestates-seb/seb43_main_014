52fe0238d30ba33d05a3635b972a1f87e36233a2
구름 <118735475+9ruem2@users.noreply.github.com>
2023-05-08 16:28:49 +0900

    ✨ Feat : userDto email 필드의 유효성검사와 유니크 검사를 함께 처리하는 코드작성

---
+++ server/src/main/java/com/cv/domain/audit/Auditable.java
@@ -0,0 +1,32 @@
+package com.cv.domain.audit;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+    @CreatedDate
+    @Column(name = "createdAt", updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "lastModifiedAt")
+    private LocalDateTime modifiedAt;
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column
+    private String modifiedBy;
+}
---
+++ server/src/main/java/com/cv/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cv.domain.user.repository;
+
+import com.cv.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
