0a8472aa4d84bbb532ca27248adf25625734465d
chaeyeon0130 <k84292330@gmail.com>
2023-05-08 17:04:00 +0900

    ✨ Feat : 로그인 인증 및 JWT 생성 로직 구현

---
+++ server/src/main/java/com/cv/global/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,88 @@
+package com.cv.global.auth.filter;
+
+//import com.cv.domain.user.entity.User;
+//import com.cv.global.auth.dto.LoginDto;
+//import com.cv.global.auth.jwt.JwtTokenizer;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.SneakyThrows;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//    private final AuthenticationManager authenticationManager;
+//    private final JwtTokenizer jwtTokenizer;
+//
+//    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenizer jwtTokenizer) {
+//        this.authenticationManager = authenticationManager;
+//        this.jwtTokenizer = jwtTokenizer;
+//    }
+//
+//    // 인증 시도
+//    @SneakyThrows
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+//
+//        UsernamePasswordAuthenticationToken authenticationToken =
+//                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+//
+//        return authenticationManager.authenticate(authenticationToken);
+//    }
+//
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request,
+//                                            HttpServletResponse response,
+//                                            FilterChain chain,
+//                                            Authentication authResult) throws ServletException, IOException {
+//        User user = (User) authResult.getPrincipal();
+//
+//        String accessToken = delegateAccessToken(user);
+//        String refreshToken = delegateAccessToken(user);
+//
+//        response.setHeader("Authorization", "Bear " + accessToken);
+//        response.setHeader("Refresh", refreshToken);
+//
+//        // JWT 생성을 AuthenticationSuccessHandler에서 해도 됨
+//        // FIXME : 프론트 단에서, 사용자 정보를 원하면 로그인 인증 후 추가 작업을 AuthenticationSuccessHandler에서 해주는 게 좋아 보임
+//        this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+//
+//        // AuthenticationFailureHandler의 onAuthenticationFailure()는 알아서 호출됨
+//    }
+//
+//    private String delegateAccessToken(User user) {
+//        Map<String, Object> claims = new HashMap<>();
+//        claims.put("username", user.getEmail());
+//        claims.put("roles", user.getRoles());
+//
+//        String subject = user.getEmail();
+//        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationHours());
+//
+//        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+//
+//        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+//
+//        return accessToken;
+//    }
+//
+//    private String delegateRefreshToken(User user) {
+//        String subject = user.getEmail();
+//        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationHours());
+//        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+//
+//        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+//
+//        return refreshToken;
+//    }
+//}
