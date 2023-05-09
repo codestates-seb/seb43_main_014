aa92a32928ccdbe6c89374e73ba7fef0124f1008
jaeyumn <wkddkgodsla@gmail.com>
2023-05-09 15:17:15 +0900

    ✨ Feat : LinkName enum으로 변경

--- server/src/main/java/com/cv/domain/cv/entity/Link.java
+++ server/src/main/java/com/cv/domain/cv/entity/Link.java
@@ -1,6 +1,7 @@
 package com.cv.domain.cv.entity;
 
 import lombok.Data;
+import lombok.Getter;
 
 import javax.persistence.*;
 
@@ -12,11 +13,25 @@ public class Link {
     @Id
     private Long linkId;
 
-    private String linkName;
+    @Enumerated(EnumType.STRING)
+    private LinkName linkName;
 
     private String linkAddress;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "CV_ID")
     private Cv cv;
+
+    public enum LinkName {
+        LINK_GITHUB("깃허브 주소"),
+        LINK_NOTION("노션 주소"),
+        LINK_BLOG("블로그 주소");
+
+        @Getter
+        private String linkSource;
+
+        LinkName(String linkSource) {
+            this.linkSource = linkSource;
+        }
+    }
 }
