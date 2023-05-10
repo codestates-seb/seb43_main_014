41835a0818d01d9793f4ea898ba92af00f8fd481
jaeyumn <wkddkgodsla@gmail.com>
2023-05-10 14:41:32 +0900

    ✨ Feat : SkillStack 관련 Repository 추가

---
+++ server/src/main/java/com/cv/domain/project/repository/ProjectSkillStackRepository.java
@@ -0,0 +1,7 @@
+package com.cv.domain.project.repository;
+
+import com.cv.domain.project.entity.ProjectSkillStack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectSkillStackRepository extends JpaRepository<ProjectSkillStack, Long> {
+}
