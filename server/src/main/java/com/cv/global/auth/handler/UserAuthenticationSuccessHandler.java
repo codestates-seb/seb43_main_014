fc01426c86308cdde3da1d5f39e728089dafb4d4
chaeyeon0130 <k84292330@gmail.com>
2023-05-11 14:33:37 +0900

    ✨ Feat : 로그인 인증 후 response body에 사용자 정보 추가

--- server/src/main/java/com/cv/global/auth/handler/UserAuthenticationSuccessHandler.java
+++ server/src/main/java/com/cv/global/auth/handler/UserAuthenticationSuccessHandler.java
@@ -16,8 +16,7 @@ public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHa
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException {
 
-        // TODO : 인증 성공 후, 사용자 정보를 response로 전송하는 작업을 여기에 해도 될 것 같음
-        // authentication에서 사용자 정보를 얻은 후, HttpServletResponse로 출력 스트림을 생성하여 response를 전송할 수 있음
+        // TODO : Refactor -> 인증 성공 후, JWT 토큰과 사용자 정보를 response로 전송하는 작업을 여기에서 함
         log.info("# Authenticated successfully!");
     }
 }
