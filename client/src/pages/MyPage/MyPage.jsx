e226b11b8c92adec29f8d3eb6acc3053e2480881
dohyun98 <kch9554@naver.com>
2023-05-11 17:41:56 +0900

    🔨 Refactor : state

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './mypage.module.css';
 import PwChange from '../../components/MyPage/PwChange/PwChange';
 import Profile from '../../components/MyPage/Profile/Profile';
@@ -6,11 +6,20 @@ import DeleteAccount from '../../components/MyPage/DeleteAccount';
 import CvList from '../../components/MyPage/CvList';
 
 const MyPage = () => {
+  const name = '도현';
+  const email = `kdohyn98@github.com`;
+  const phone = '01012341234';
+  const [inputs, setInputs] = useState({
+    name: name,
+    email: email,
+    phone: phone,
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.mypageItem}>
         <h2 className={styles.profileTitle}>기본정보</h2>
-        <Profile />
+        <Profile inputs={inputs} setInputs={setInputs} />
       </div>
       <div className={styles.mypageCv}>
         <h2>이력서</h2>
@@ -25,7 +34,7 @@ const MyPage = () => {
       </div>
       <div className={styles.mypageItem}>
         <h2>계정 삭제</h2>
-        <DeleteAccount />
+        <DeleteAccount inputs={inputs} />
       </div>
     </div>
   );
