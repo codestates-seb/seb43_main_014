ef1b3a3d9663e4af9ea97ae4c8a8ec103c23bb33
dongwook98 <ehddnr8563@gmail.com>
2023-05-15 14:10:17 +0900

    ⚠ fix : 로그인 기능 구현
    - 토큰 인증 기반 방식은 굳이 쿠키에 넣을 필요 없음!
    - 로그인 요청 시 서버에서 받아온 유저 데이터를 로컬 스토리지에 저장

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -6,22 +6,21 @@ import Button from '../../components/common/Button/Button';
 import LabelInput from '../../components/common/LabelInput/LabelInput';
 import { Link, useNavigate } from 'react-router-dom';
 import Oauth from '../../components/common/Oauth/Oauth';
-import { useRecoilValue, useSetRecoilState, useRecoilState } from 'recoil';
-import { isLoginSelector, tokenState } from '../../recoil/TokenAtom';
+import { useRecoilState } from 'recoil';
+import { isLoginState, tokenState } from '../../recoil/TokenAtom';
+import { userState } from '../../recoil/userAtom';
 import axios from 'axios';
-import { useCookies } from 'react-cookie';
 import { validate } from '../../utils/validate-login';
 import Alert from '../../components/common/Alert/Alert';
 
 export default function Login() {
   const navigate = useNavigate();
-  const Tokenvalue = useRecoilValue(tokenState);
 
-  const [cookies, setCookie] = useCookies(['token']); // useCookies
-
-  const setToken = useSetRecoilState(tokenState);
-  const [isLogin, setIsLogin] = useRecoilState(isLoginSelector);
+  const [token, setToken] = useRecoilState(tokenState);
+  const [isLogin, setIsLogin] = useRecoilState(isLoginState);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
   console.log(isLogin); // 로그인시 : false -> true
+  console.log('userInfo : ', userInfo);
 
   // InputValueState
   const [form, setForm] = useState({
@@ -54,15 +53,14 @@ export default function Login() {
           },
         )
         .then((res) => {
-          console.log(res.headers);
-          console.log(res);
-
           // 토큰 값을 Recoil 상태로 업데이트합니다.
           const token = res.headers.authorization.split(' ')[1]; // "Bearer " 부분을 제외한 토큰 값만 추출
           setToken(token); // 토큰을 리코일 상태에 저장
-          console.log(Tokenvalue);
-          setCookie('jwt_token', token, { path: '/' }); // 토큰을 쿠키에 저장 // document.cookie
-          alert('로그인 성공!');
+          setIsLogin(true); // 로그인 상태 리코일 상태에 저장
+          setUserInfo(res.data); // 유저 데이터 리코일 상태에 저장
+          localStorage.setItem('jwt_token', token);
+          const userData = res.data;
+          localStorage.setItem('user_info', JSON.stringify(userData));
           navigate('/');
         })
         .catch(() => {
