14ab35939c02985c9998f95a480e39300427949a
dohyun98 <kch9554@naver.com>
2023-05-18 15:16:52 +0900

    🔨 Refactor : img 수정

--- client/src/components/MyPage/Profile/ProfileCard/ProfileCard.jsx
+++ client/src/components/MyPage/Profile/ProfileCard/ProfileCard.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import styles from './profileCard.module.css';
 
 const ProfileCard = ({ userData, setInfoUpdata }) => {
-  const { name, email, phone } = userData;
+  const { name, email, phone, profieImage } = userData;
 
   return (
     <>
       <div className={styles.proCard}>
         <div className={styles.userInfo}>
           <div className={styles.profilePic}>
-            <img
-              className={styles.pic}
-              src="https://mediaim.expedia.com/localexpert/1391601/fe50a3dc-b95f-4815-a331-05cbbc16d855.jpg?impolicy=resizecrop&rw=1005&rh=565"
-              alt="profileImg"
-            />
+            <img className={styles.pic} src={profieImage} alt="profileImg" />
           </div>
           <div className={styles.proInfo}>
             <span className={styles.info}>{name}</span>
