4b56075ee5e26c549103bf470604a97027c2d41d
dohyun98 <kch9554@naver.com>
2023-05-23 15:37:38 +0900

    ✨ Feat : 로딩페이지, 페이지네이션 추가

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -5,16 +5,21 @@ import Profile from '../../components/MyPage/Profile/Profile';
 import DeleteAccount from '../../components/MyPage/DeleteAccount';
 import CvList from '../../components/MyPage/CvList';
 import axios from 'axios';
+import NoCvList from '../../components/MyPage/NoCvList/NoCvList';
+import Spinner from '../../components/common/Spinner';
 
 const MyPage = () => {
   const token = localStorage.getItem('jwt_token');
   const user = localStorage.getItem('user_info');
   const { userId } = JSON.parse(user);
   const [userData, setUserData] = useState('');
-  const { cvs } = userData;
+  const [pageData, setPageData] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   console.log('userId', userId);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(
         `http://ec2-13-209-35-225.ap-northeast-2.compute.amazonaws.com:8080/user/my-page/${userId}?page=1`,
@@ -24,36 +29,92 @@ const MyPage = () => {
           },
         },
       )
-      .then((response) => setUserData(response.data))
+      .then((response) => {
+        setUserData(response.data);
+        setIsLoading(false);
+      })
       .catch((error) => {
         console.log(error);
       });
   }, [token, userId]);
-  console.log(userData);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://ec2-13-209-35-225.ap-northeast-2.compute.amazonaws.com:8080/user/my-page/${userId}/cvs?page=${currentPage}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      .then((response) => setPageData(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [currentPage]);
+  console.log(pageData);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= pageData.totalPages; i++) {
+      const isActive = i === currentPage;
+      pageNumbers.push(
+        <button
+          className={`${styles.pageNum} ${isActive ? styles.checkNum : ''}`}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </button>,
+      );
+    }
+    return pageNumbers;
+  };
+  console.log(currentPage);
   return (
-    <div className={styles.container}>
-      <div className={styles.mypageItem}>
-        <h2 className={styles.profileTitle}>기본정보</h2>
-        <Profile userData={userData} setUserData={setUserData} />
-      </div>
-      <div className={styles.mypageCv}>
-        <h2>이력서</h2>
-        {/* {cvs.latestCvs.map((cv) => (
-          <CvList cv={cv} key={cv[cv.length]} cvs={cvs} />
-        ))} */}
-        <div className={styles.cvList}>
-          <button>새이력서 추가</button>
-        </div>
-      </div>
-      <div className={styles.mypageItem}>
-        <h2>비밀번호</h2>
-        <PwChange />
-      </div>
-      <div className={styles.mypageItem}>
-        <h2>계정 삭제</h2>
-        <DeleteAccount />
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className={styles.container}>
+            <div className={styles.mypageItem}>
+              <h2 className={styles.profileTitle}>기본정보</h2>
+              <Profile userData={userData} setUserData={setUserData} />
+            </div>
+            <div className={styles.mypageCv}>
+              <h2>이력서</h2>
+              <div className={styles.cvContainers}>
+                <div className={styles.cvContainer}>
+                  {!pageData.latestCvs ? (
+                    <NoCvList />
+                  ) : (
+                    pageData.latestCvs.map((cv) => (
+                      <CvList cv={cv} key={cv.cvId} />
+                    ))
+                  )}
+                </div>
+                <div className={styles.pageNums}>{renderPageNumbers()}</div>
+              </div>
+              <div className={styles.cvList}>
+                <button>새이력서 추가</button>
+              </div>
+            </div>
+            <div className={styles.mypageItem}>
+              <h2>비밀번호</h2>
+              <PwChange />
+            </div>
+            <div className={styles.mypageItem}>
+              <h2>계정 삭제</h2>
+              <DeleteAccount />
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
