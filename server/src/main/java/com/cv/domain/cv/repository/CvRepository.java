aeba52cc2dca371dc4203728382179c43ee12473
구름 <118735475+9ruem2@users.noreply.github.com>
2023-05-24 15:28:36 +0900

    ✨ Feat : repository 쿼리문 추가

--- server/src/main/java/com/cv/domain/cv/repository/CvRepository.java
+++ server/src/main/java/com/cv/domain/cv/repository/CvRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface CvRepository extends JpaRepository<Cv, Long> {
     // JPQL - 엔티티 객체를 대상으로 질의하는 쿼리
-    // 특정 사용자, 최신순으로 이력서 조회
+    // 특정 사용자, 최신순으로 이력서 조회, 삭제된 이력서는 리스트에서 제외
     @Query(value = "select cv from Cv cv where cv.user.userId = :userId and cv.isDelete = false")
     Page<Cv> findByUserIdFromRecently (@Param("userId") Long userId, Pageable pageable); // (1)
 
