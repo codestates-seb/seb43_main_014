c5f5d82ad4f32e9bf84fea0762177d1eda5cef44
구름 <118735475+9ruem2@users.noreply.github.com>
2023-05-24 16:09:07 +0900

    ✨ Feat : response 추가

--- server/src/main/java/com/cv/domain/user/dto/mypage/UserPatchResponseDto.java
+++ server/src/main/java/com/cv/domain/user/dto/mypage/UserPatchResponseDto.java
@@ -15,7 +15,7 @@ public class UserPatchResponseDto {
     private String phone;
     @Schema(description = "프로필 이미지", example = "user/profile/image/smile")
     private String profileImage;
-    @Schema(description = "프로필 이미지", example = "2023-05-23")
+    @Schema(description = "마지막 수정일자", example = "2023-05-23")
     private LocalDate modifiedAt;
     @Schema(description = "회원가입 일자", example = "2023-05-23")
     private LocalDate createdAt;
