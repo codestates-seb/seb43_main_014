b1e852ad1198e5bf5b57ea736353a63f5f7749bf
jaeyumn <wkddkgodsla@gmail.com>
2023-05-25 19:33:27 +0900

    ✨ Feat : 이력서 수정 link 추가

--- client/src/components/MyPage/Btn/Btn.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './btn.module.css';
-
-const Btn = (props) => {
-  return <button className={styles.btn}>{props.btnName}</button>;
-};
-
-export default Btn;
--- client/src/components/common/FormBox/FormBox.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './FormBox.module.css';
-
-export default function FormBox({ children }) {
-  return <section className={styles.right}>{children}</section>;
-}
