fdfeaaed3de5a1c39752ebf11e8725ee1fc74c02
dongwook98 <ehddnr8563@gmail.com>
2023-05-25 20:09:56 +0900

    ✨ Feat : Not Found 페이지에 홈 버튼 추가

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../../images/rocket.png';
 import styles from './NotFound.module.css';
+import { Link } from 'react-router-dom';
+import Btn from '../../components/MyPage/Btn/Btn';
 
 export default function NotFound() {
   return (
@@ -8,6 +10,9 @@ export default function NotFound() {
       <h1>로켓CV</h1>
       <img src={logo} alt="로켓CV로고" />
       <h1>아직 준비중인 페이지에요! 😅</h1>
+      <Link to="/">
+        <Btn btnName="홈 돌아가기" />
+      </Link>
     </div>
   );
 }
