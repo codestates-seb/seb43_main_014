a17e822c22756c8bfea5ea010b4d8ab75f7a7097
orihiro <kimhijin1@naver.com>
2023-06-19 23:55:35 +0900

    🎨 Design : Main Page react-fullpage 추가

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -8,32 +8,59 @@ import CvSample from '../components/Cv/CvSample';
 import { useRecoilState } from 'recoil';
 import { isLoginState } from '../recoil/AuthAtom';
 import { CvContentAtom } from '../recoil/CvContentAtom';
+import { SectionsContainer, Section } from 'react-fullpage';
+import Footer from '../components/Cv/Footer';
 
 const Main = () => {
   const [isLogin, setIsLogin] = useRecoilState(isLoginState);
   const [cvContent, setCvContent] = useRecoilState(CvContentAtom);
+  const options = {
+    activeClass: 'active', // the class that is appended to the sections links
+    anchors: ['One', 'Two', 'Three'], // the anchors for each sections
+    arrowNavigation: true, // use arrow keys
+    delay: 900, // the scroll animation speed
+    navigation: false, // use dots navigatio
+    scrollBar: false, // use the browser default scrollbar
+  };
 
   setCvContent('');
   if (isLogin)
     return (
       <>
         <Container>
-          <Link to="/">
-            <img className="Logo" src={Logo} alt="로고 이미지" />
-          </Link>
-          <span>신입 개발자의 이력서 작성, 막막하기만 하다구요?</span>
-          <span>이력서를 가장 빠르고 쉽게 제작하는 방법!</span>
-          <Link to="/create-cv">
-            <StyledButton>이력서 작성하기</StyledButton>
-          </Link>
-          <h3>사용자가 말하는 로켓CV 사용기!</h3>
-          <div className="row">
-            <ReviewCard />
-          </div>
-          <EventTap />
-          <div className="tem"></div>
-          <h3>전문적인 이력서 템플릿을 이용해보세요!</h3>
-          <CvSample />
+          <SectionsContainer {...options}>
+            <Section>
+              <div className="top">
+                <img className="Logo" src={Logo} alt="로고 이미지" />
+
+                <div>
+                  <span>신입 개발자의 이력서 작성, 막막하기만 하다구요?</span>
+                </div>
+                <div>
+                  <span>이력서를 가장 빠르고 쉽게 제작하는 방법!</span>
+                </div>
+                <div>
+                  <Link to="/create-cv">
+                    <StyledButton>이력서 작성하기</StyledButton>
+                  </Link>
+                </div>
+              </div>
+            </Section>
+            <Section>
+              <div className="mid">
+                <h3>사용자가 말하는 로켓CV 사용기!</h3>
+                <div className="row">
+                  <ReviewCard />
+                </div>
+                <EventTap />
+              </div>
+            </Section>
+            <Section>
+              <div className="tem"></div>
+              <h3>전문적인 이력서 템플릿을 이용해보세요!</h3>
+              <CvSample />
+            </Section>
+          </SectionsContainer>
         </Container>
       </>
     );
@@ -41,22 +68,33 @@ const Main = () => {
     return (
       <>
         <Container>
-          <Link to="/">
-            <img className="Logo" src={Logo} alt="로고 이미지" />
-          </Link>
-          <span>신입 개발자의 이력서 작성, 막막하기만 하다구요?</span>
-          <span>이력서를 가장 빠르고 쉽게 제작하는 방법!</span>
-          <Link to="/login">
-            <StyledButton>이력서 작성하기</StyledButton>
-          </Link>
-          <h3>사용자가 말하는 로켓CV 사용기!</h3>
-          <div className="row">
-            <ReviewCard />
-          </div>
-          <EventTap />
-          <div className="tem"></div>
-          <h3>전문적인 이력서 템플릿을 이용해보세요!</h3>
-          <CvSample />
+          <SectionsContainer {...options}>
+            <Section>
+              <Link to="/">
+                <img className="Logo" src={Logo} alt="로고 이미지" />
+              </Link>
+              <span>신입 개발자의 이력서 작성, 막막하기만 하다구요?</span>
+              <span>이력서를 가장 빠르고 쉽게 제작하는 방법!</span>
+              <Link to="/login">
+                <StyledButton>이력서 작성하기</StyledButton>
+              </Link>
+            </Section>
+            <Section>
+              <div className="mid">
+                <h3>사용자가 말하는 로켓CV 사용기!</h3>
+                <div className="row">
+                  <ReviewCard />
+                </div>
+                <EventTap />
+              </div>
+            </Section>
+            <Section>
+              <div className="tem">
+                <h3>전문적인 이력서 템플릿을 이용해보세요!</h3>
+                <CvSample />
+              </div>
+            </Section>
+          </SectionsContainer>
         </Container>
       </>
     );
@@ -66,10 +104,10 @@ const Main = () => {
 export default Main;
 
 const StyledButton = styled.button`
-  width: 10rem;
-  height: 3.5rem;
-  margin: 2rem 0 10rem 0;
-  font-size: 0, 8rem;
+  width: 15rem;
+  height: 6rem;
+  margin: 20rem 0 0 18rem;
+  font-size: 1.2rem;
   cursor: pointer;
   border: none;
   border-radius: 0.3rem var(--puple100);
@@ -81,7 +119,8 @@ const StyledButton = styled.button`
   }
 `;
 
-const Container = styled.main`
+const Container = styled.div`
+  overflow: hidden;
   margin-top: 3rem;
   width: 100%;
   max-width: 100%;
@@ -98,16 +137,30 @@ const Container = styled.main`
   .row {
     display: flex;
     flex-direction: row;
-    margin-top: 2rem;
+  }
+  .top {
+    margin-left: 5rem;
+  }
+  .mid {
+    margin-left: 7rem;
   }
   h3 {
     font-size: 1.8rem;
   }
   .Logo {
-    margin: 2rem 0 3rem 0;
+    margin-left: 15rem;
+    margin-bottom: 5rem;
     width: 20rem;
-  }
-  .tem {
-    margin-top: 8rem;
+    opacity: 0.8;
+    transition: 0.3s ease-in-out;
+    animation: sildeSideimg 2s forwards 1s;
+    @keyframes sildeSideimg {
+      100% {
+        border-radius: 100px 0px 100px 0px;
+      }
+    }
+    :hover {
+      transform: rotateY(-20deg);
+    }
   }
 `;
